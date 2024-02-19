28bcb1cae85751a545a86b0cec842e03da3bac62
flixding <felix.dingler@stud.uni-due.de>
2024-02-19 13:28:29 +0100

    Added Code documentation

--- frontend/src/pages/Projects/NoteBot/notes/App.jsx
+++ frontend/src/pages/Projects/NoteBot/notes/App.jsx
@@ -1,14 +1,16 @@
 import * as React from "react";
-import Note from "./note";
-import {Stack} from "@mui/material";
-import { useState } from "react";
+import Note from "./note"; // Importing the Note component
+import { Stack } from "@mui/material"; // Importing Stack component from Material-UI
+import { useState } from "react"; // Importing useState hook from React
 
 export default function App({ notesInfo, uid }) {
+    // Initializing state for notes using useState hook
     const [notes, setNotes] = useState({
-        message: "Server not connected",
-        notes: notesInfo || [],
+        message: "Server not connected", // Default message if notesInfo is not provided
+        notes: notesInfo || [], // Initial array of notes, defaulting to an empty array if notesInfo is not provided
     });
 
+    // Function to remove a note from the notes array
     const removeNote = (oldNote) => {
         setNotes((prevNotes) => ({
             ...prevNotes,
@@ -17,14 +19,17 @@ export default function App({ notesInfo, uid }) {
     };
 
     return (
-            <Stack direction="row" spacing={2} sx={{ padding: '4px 0 4px 0', overflowX: 'auto'}}>
-                {notes.notes.length > 0 ? (
-                    notes.notes.map((noteId) => (
-                        <Note key={noteId} noteId={noteId} userId={uid} removeNote={removeNote} />
-                    ))
-                ) : (
-                        <i>No notes yet.</i>
-                )}
-            </Stack>
+        // Displaying notes in a horizontal stack layout
+        <Stack direction="row" spacing={2} sx={{ padding: '4px 0 4px 0', overflowX: 'auto'}}>
+            {notes.notes.length > 0 ? (
+                // Mapping through the notes array and rendering Note component for each note
+                notes.notes.map((noteId) => (
+                    <Note key={noteId} noteId={noteId} userId={uid} removeNote={removeNote} />
+                ))
+            ) : (
+                // Displaying a message if there are no notes
+                <i>No notes yet.</i>
+            )}
+        </Stack>
     );
 }
