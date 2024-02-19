98cbcc404ea8e15ad79acaef553f4753a8eef8a9
gioth4 <147994935+gioth4@users.noreply.github.com>
2024-02-19 16:20:18 +0100

    Documentation | \Create\sections\app.jsx

--- frontend/src/pages/Projects/NoteBot/Create/sections/app.jsx
+++ frontend/src/pages/Projects/NoteBot/Create/sections/app.jsx
@@ -4,28 +4,37 @@ import Section from "./section.jsx";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Grid from '@mui/material/Grid';
 
-export default function Sections ({counter, addSection, addLayout, addWidget, setWidgetContent, initialNote}) {
+// Sections component responsible for rendering multiple sections
+export default function Sections({counter, addSection, addLayout, addWidget, setWidgetContent, initialNote}) {
+    // Function to add a new section
     const handleShowLayoutSelector = () => {
         addSection();
     };
-    return (<div>
-        <Grid item container direction="column">
-            <Grid item>
-                {Array(counter).fill().map((_, index) => (
-                            <Section
-                                index={index}
-                                addLayout={addLayout}
-                                addWidget={addWidget}
-                                setWidgetContent={setWidgetContent}
-                                initialSection={initialNote.section[index]}
-                            />
-                ))}
+
+    return (
+        <div>
+            {/* Container for rendering multiple sections */}
+            <Grid item container direction="column">
+                <Grid item>
+                    {/* Map through sections and render Section components */}
+                    {Array(counter).fill().map((_, index) => (
+                        <Section
+                            key={index}
+                            index={index}
+                            addLayout={addLayout}
+                            addWidget={addWidget}
+                            setWidgetContent={setWidgetContent}
+                            initialSection={initialNote.section[index]}
+                        />
+                    ))}
+                </Grid>
+                <Grid item sx={{margin: 'auto'}}>
+                    {/* Button to add a new section */}
+                    <IconButton aria-label="add section" onClick={handleShowLayoutSelector} size="large">
+                        <AddCircleIcon fontSize="large"/>
+                    </IconButton>
+                </Grid>
             </Grid>
-            <Grid item sx={{margin: 'auto'}}>
-                <IconButton aria-label="add section" onClick={handleShowLayoutSelector} size="large">
-                    <AddCircleIcon fontSize="large"/>
-                </IconButton>
-            </Grid>
-        </Grid>
-    </div>)
-};
+        </div>
+    );
+}
