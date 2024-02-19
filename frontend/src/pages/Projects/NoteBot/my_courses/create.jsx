3d1d570a1668765d31436083e36f4955c974ce8f
flixding <felix.dingler@stud.uni-due.de>
2024-02-19 17:25:23 +0100

    Documentation | my_courses\create.jsx

--- frontend/src/pages/Projects/NoteBot/my_courses/create.jsx
+++ frontend/src/pages/Projects/NoteBot/my_courses/create.jsx
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 import { Box, FormControl, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button } from "@mui/material";
 import { createCourse } from "../utils/api.js";
 
+// Component for creating a new course dialog
 export const CreateCourseDialog = ({ isOpen, onClose, courses, user_id, updateCourses }) => {
+    // State for managing the course title
     const [courseTitle, setCourseTitle] = useState("");
+
+    // Function to handle adding a new course
     const handleAddCourse = async () => {
         try {
+            // Create a new course using the API function
             const newCourse = await createCourse(user_id, courseTitle);
+            
             // Update the courses list state in app.jsx
             updateCourses(newCourse);
 
-            // Close the dialog
             onClose();
         } catch (error) {
+            // Log error if failed to create a new course
             console.log("Failed to create a new course:", error);
         }
     };
+
     return (
         <Dialog onClose={onClose} open={isOpen} fullWidth>
             <DialogTitle>Add New Course</DialogTitle>
@@ -23,6 +30,7 @@ export const CreateCourseDialog = ({ isOpen, onClose, courses, user_id, updateCo
                 <Box sx={{height:12}}/>
 
                 <FormControl fullWidth>
+                    {/* Input field for entering the course title */}
                     <TextField
                         label="Course Title"
                         value={courseTitle}
@@ -33,12 +41,14 @@ export const CreateCourseDialog = ({ isOpen, onClose, courses, user_id, updateCo
                 </FormControl>
             </DialogContent>
             <DialogActions sx={{ padding: 2 }}>
+                {/* Cancel button */}
                 <Button
                     onClick={onClose}
                     variant="outlined"
                 >
                     Cancel
                 </Button>
+                {/* Add button */}
                 <Button
                     onClick={handleAddCourse}
                     variant="contained"
