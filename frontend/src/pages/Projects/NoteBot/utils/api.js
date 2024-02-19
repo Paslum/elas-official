72ca65feb7e4c66c980738d82a96905dbcc34f29
flixding <felix.dingler@stud.uni-due.de>
2024-02-19 16:07:11 +0100

    Documentation | utils/api.js

--- frontend/src/pages/Projects/NoteBot/utils/api.js
+++ frontend/src/pages/Projects/NoteBot/utils/api.js
@@ -1,14 +1,19 @@
-import {Backend, setAuthToken} from "../../../../utils/apiConfig";
+import { Backend, setAuthToken } from "../../../../utils/apiConfig";
 
 /** Users **/
+
+// Function to get user information by user ID
 export const getUserInfo = async (userId) => {
   try {
+    // Request user info from the backend
     const response = await Backend.get(`/notebot/users/${userId}`);
+    // Extracting message and user data from response
     const {
       data: { message, user },
     } = response;
     return { message, user };
   } catch (err) {
+    // Handling errors and returning default data if server connection fails
     console.log(err);
     return {
       message: "Server not connected",
@@ -22,14 +27,19 @@ export const getUserInfo = async (userId) => {
 };
 
 /** Courses **/
+
+// Function to get all courses
 export const getAllCourses = async () => {
   try {
+    // Request all courses from the backend
     const response = await Backend.get(`/notebot/courses`);
+    // Extracting message and course data from response
     const {
       data: { message, course },
     } = response;
     return { message, course };
   } catch (err) {
+    // Handling errors and returning default data if server connection fails
     console.log(err);
     return {
       message: "Server not connected",
@@ -40,14 +50,19 @@ export const getAllCourses = async () => {
     };
   }
 };
+
+// Function to get courses by user ID
 export const getCoursesByUserId = async (userId) => {
   try {
+    // Request courses by user ID from the backend
     const response = await Backend.get(`/notebot/course/${userId}`);
+    // Extracting message and course data from response
     const {
       data: { message, course },
     } = response;
     return { message, course };
   } catch (err) {
+    // Handling errors and returning default data if server connection fails
     console.log(err);
     return {
       message: "Server not connected",
@@ -58,14 +73,19 @@ export const getCoursesByUserId = async (userId) => {
     };
   }
 };
+
+// Function to get courses by title
 export const getCoursesByTitle = async (title) => {
   try {
+    // Request courses by title from the backend
     const response = await Backend.get(`/notebot/course/search/${title}`);
+    // Extracting message and course data from response
     const {
       data: { message, course },
     } = response;
     return { message, course };
   } catch (err) {
+    // Handling errors and returning default data if server connection fails
     console.log(err);
     return {
       message: "Server not connected",
@@ -76,21 +96,29 @@ export const getCoursesByTitle = async (title) => {
     };
   }
 };
+
+// Function to delete a course
 export const deleteCourse = async (course_id) => {
   try {
+    // Sending delete course request to the backend
     const response = await Backend.delete(`/notebot/course/delete/${course_id}`)
     const data = await response.data;
 
     return data.course;
   } catch (error) {
+    // Handling errors
     throw new Error(`Failed to delete course. Error: ${error.message}`);
   }
 };
+
+// Function to create a course
 export const createCourse = async (user, title) => {
   try {
+    // Sending create course request to the backend
     const response = await Backend.post(`/notebot/course/`, { uid: user, title: title });
     const data = await response.data;
 
+    // Processing response and returning course information
     if (data.message && data.message.includes('created successfully')) {
       const courseInfo = {
         title: title,
@@ -102,29 +130,39 @@ export const createCourse = async (user, title) => {
       throw new Error('Failed to extract course information from the response.');
     }
   } catch (error) {
+    // Handling errors
     throw new Error(`Failed to create a new course. Error: ${error.message}`);
   }
 };
+
+// Function to update a course
 export const updateCourse = async (courseId, title) => {
   try {
+    // Sending update course request to the backend
     const response = await Backend.put(`/notebot/course/update`, { courseId: courseId, title: title })
     const data = await response.data;
 
     return data.course;
   } catch (error) {
+    // Handling errors
     throw new Error(`Failed to update course. Error: ${error.message}`);
   }
 };
 
 /** Notes **/
+
+// Function to get a note by note ID
 export const getNoteById = async (noteId) => {
   try {
+    // Request note by note ID from the backend
     const response = await Backend.get(`/notebot/note/${noteId}`);
+    // Extracting message and note data from response
     const {
       data: { message, note },
     } = response;
     return { message, note };
   } catch (err) {
+    // Handling errors and returning default data if server connection fails
     console.log(err);
     return {
       message: "Server not connected",
@@ -136,8 +174,10 @@ export const getNoteById = async (noteId) => {
   }
 };
 
+// Function to get note content by note ID
 export const getNoteContentById = async (noteId) => {
   try {
+    // Requesting note, course, sections, and widgets information from the backend
     const responseNote = await Backend.get(`/notebot/note/${noteId}`);
     const responseCourse = await Backend.get(`/notebot/courses`);
     const filteredCourse = responseCourse.data.course.find(course => course.notes.includes(noteId));
@@ -150,6 +190,7 @@ export const getNoteContentById = async (noteId) => {
       return await Backend.get(`/notebot/widget/${widget}`);
     };
 
+    // Processing response data and returning note information
     const NoteInfo = {
       message: responseNote.data.message,
       title: responseNote.data.note.title,
@@ -171,15 +212,18 @@ export const getNoteContentById = async (noteId) => {
 
     return NoteInfo;
   } catch (error) {
+    // Handling errors and returning default data if server connection fails
     console.log(error);
     return {
       message: "Server not connected",
     };
   }
-}
+};
 
+// Function to create a note
 export const createNote = async (userId, title, course, layout, widgets) => {
   try {
+    // Sending create note request to the backend
     const response = await Backend.post(`/notebot/note`, { uid: userId, title: title, courseId: course });
     const data = await response.data;
     for (let i = 0; i < layout.layout.length; i++) {
@@ -191,29 +235,36 @@ export const createNote = async (userId, title, course, layout, widgets) => {
       }
     }
 
+    // Processing response and returning data
     if (data.message && data.message.includes('created successfully')) {
       return response.data;
     } else {
       throw new Error('Failed to extract note information from the response.');
     }
   } catch (error) {
+    // Handling errors
     throw new Error(`Failed to create a new note. Error: ${error.message}`);
   }
 };
 
+// Function to delete a note
 export const deleteNote = async (noteId) => {
   try {
+    // Sending delete note request to the backend
     const response = await Backend.delete(`/notebot/note/delete/${noteId}`)
     const data = await response.data;
 
     return data.note;
   } catch (error) {
+    // Handling errors
     throw new Error(`Failed to delete Note. Error: ${error.message}`);
   }
 };
 
+// Function to get favorite notes by user ID
 export const getFavNotesByUserId = async (userId) => {
   try {
+    // Request favorite notes by user ID from the backend
     const response = await Backend.get(`/notebot/favorite/${userId}`)
     const {
       data: { message, note },
@@ -221,38 +272,48 @@ export const getFavNotesByUserId = async (userId) => {
 
     return { message, note };
   } catch (error) {
+    // Handling errors
     throw new Error(`Failed to fetch Favorite Notes. Error: ${error.message}`);
 
   }
 };
 
+// Function to check if a note is favorited by a user
 export const isFavNote = async (userId, note) => {
   try {
+    // Checking if a note is favorited by a user
     const response = await Backend.get(`/notebot/isFavNote/?userId=${userId}&note=${note}`);
     return response.data.isFav;
   } catch (error) {
-
+    // Handling errors
+    console.log(error); // Log the error for debugging
   }
 };
 
+// Function to add a note to favorites
 export const addFavNote = async (userId, note) => {
   try {
+    // Sending add favorite note request to the backend
     const response = await Backend.post(`/notebot/favorite/`, { userId: userId, note: note });
     const data = await response.data;
 
     return data.message;
   } catch (err) {
+    // Handling errors
     throw new Error(`Failed to favorite Notes. Error: ${error.message}`);
   }
 }
 
+// Function to remove a note from favorites
 export const remFavNote = async (userId, note) => {
   try {
+    // Sending remove favorite note request to the backend
     const response = await Backend.post(`/notebot/remfavorite/`, { userId: userId, note: note });
     const data = await response.data;
 
     return data.message;
   } catch (err) {
+    // Handling errors
     throw new Error(`Failed to de favorite Notes. Error: ${error.message}`);
   }
 }
