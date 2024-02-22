237f74ac93a4d0e6d5abdd46cdf0b4f8389c90fb
Paslum <53943943+Paslum@users.noreply.github.com>
2024-02-22 16:03:20 +0100

    Documentation| course.controller

--- backend/7-notebot/src/controllers/course.controller.js
+++ backend/7-notebot/src/controllers/course.controller.js
@@ -1,63 +1,120 @@
 const db = require("../models");
 const courseModel = db.course;
 
-//get all courses
+/***************** START: COURSE MANAGEMENT API *****************
+ * @documentation
+ *
+ * @function getAllCourses
+ * The `getAllCourses` function is an asynchronous function that retrieves
+ * all courses from the database and sends a response with the found courses
+ * or a message indicating that no courses were found.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of fetching courses.
+ */
 export const getAllCourses = async (req, res) => {
     try {
-        const foundCourses = await courseModel.find(); //loading every course into const
-        if (foundCourses) { //if at least one course found, return message and all courses
-            return res.status(200).send({message: `Courses found!`, course: foundCourses.map((course) => course.toObject({ getters: true}))});
-        } //if not return message
-        return res.status(200).send({message: `No Courses found!`});
+        const foundCourses = await courseModel.find();
+        if (foundCourses) {
+            return res.status(200).send({
+                message: `Courses found!`,
+                course: foundCourses.map((course) => course.toObject({ getters: true }))
+            });
+        }
+        return res.status(200).send({ message: `No Courses found!` });
     } catch (err) {
-        return res.status(500).send({message: `Fetching courses failed, please try again later.`});
+        return res.status(500).send({ message: `Fetching courses failed, please try again later.` });
     }
 };
 
-//Get courses by userId
+/**
+ * @function getCoursesByUserId
+ * The `getCoursesByUserId` function is an asynchronous function that retrieves
+ * courses associated with a specific user ID from the database and sends a response
+ * with the found courses or a message indicating that no courses were found.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of fetching courses by user ID.
+ */
 export const getCoursesByUserId = async (req, res) => {
     const userId = req.params.user_id;
     try {
-        const foundCourses = await courseModel.find({userId: userId});
+        const foundCourses = await courseModel.find({ userId: userId });
         if (foundCourses) {
-            return res.status(200).send({message: `Courses found!`, course: foundCourses.map((course) => course.toObject({ getters: true}))});
+            return res.status(200).send({
+                message: `Courses found!`,
+                course: foundCourses.map((course) => course.toObject({ getters: true }))
+            });
         }
-        return res.status(200).send({message: `No Courses found!`});
+        return res.status(200).send({ message: `No Courses found!` });
     } catch (err) {
-        return res.status(500).send({ message: `Error saving user to your MongoDB database` });
+        return res.status(500).send({ message: `Error fetching courses from the database` });
     }
 };
 
-//Get courses by Title -> possible search function
+/**
+ * @function getCoursesByTitle
+ * The `getCoursesByTitle` function is an asynchronous function that retrieves
+ * courses from the database based on the provided title search parameter and
+ * sends a response with the found courses or a message indicating that no
+ * courses were found matching the search parameter.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of fetching courses by title.
+ */
 export const getCoursesByTitle = async (req, res) => {
     const searchParam = req.params.searchParam;
     try {
-        const foundCourses = await courseModel.find({title: {$regex: searchParam}});
+        const foundCourses = await courseModel.find({ title: { $regex: searchParam } });
         if (foundCourses) {
-            return res.status(200).send({message: `Courses found!`, course: foundCourses.map((course) => course.toObject({ getters: true}))});
+            return res.status(200).send({
+                message: `Courses found!`,
+                course: foundCourses.map((course) => course.toObject({ getters: true }))
+            });
         }
-        return res.status(200).send({message: `No Courses found!`});
+        return res.status(200).send({ message: `No Courses found!` });
     } catch (err) {
-        return res.status(500).send({ message: `Error saving user to your MongoDB database` });
+        return res.status(500).send({ message: `Error fetching courses from the database` });
     }
 };
 
-//Delete course by id
+/**
+ * @function deleteCourse
+ * The `deleteCourse` function is an asynchronous function that deletes a course
+ * from the database based on the provided course ID and sends a response indicating
+ * the success or failure of the deletion operation.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of deleting the course.
+ */
 export const deleteCourse = async (req, res) => {
-  const course_id = req.params.course_id;
-  try {
-      const foundCourse = await courseModel.findOne({_id: course_id});
-      if (foundCourse) {
-          await courseModel.deleteOne({_id: course_id});
-          return res.status(200).send({message: `Course deleted!`});
-      }
-      return res.status(500).send({ message: `Cannot delete Course! Course does not exist.` });
-  } catch (err) {
-      return res.status(500).send({ message: `Couldn't delete Course!` });
-  }
+    const course_id = req.params.course_id;
+    try {
+        const foundCourse = await courseModel.findOne({ _id: course_id });
+        if (foundCourse) {
+            await courseModel.deleteOne({ _id: course_id });
+            return res.status(200).send({ message: `Course deleted!` });
+        }
+        return res.status(500).send({ message: `Cannot delete Course! Course does not exist.` });
+    } catch (err) {
+        return res.status(500).send({ message: `Couldn't delete Course!` });
+}
 };
 
-//Create a new course
+/**
+ * @function createCourse
+ * The `createCourse` function is an asynchronous function that creates a new course
+ * and saves it to the database, then sends a response indicating the success or failure
+ * of the creation operation.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of creating the course.
+ */
 export const createCourse = async (req, res) => {
     try {
         let course = new courseModel({
@@ -70,19 +127,31 @@ export const createCourse = async (req, res) => {
             _id: course._id,
         });
     } catch (err) {
-        return res.status(500).send({ message: `Error saving course to DB`});
+        return res.status(500).send({ message: `Error saving course to DB` });
     }
 };
-//Change course title
+
+/**
+ * @function updateCourse
+ * The `updateCourse` function is an asynchronous function that updates the title of a course
+ * based on the provided course ID and sends a response indicating the success or failure
+ * of the update operation.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of updating the course.
+ */
 export const updateCourse = async (req, res) => {
     const courseId = req.body.courseId;
     try {
-        const foundCourse = await courseModel.findOne({_id: courseId});
+        const foundCourse = await courseModel.findOne({ _id: courseId });
         if (foundCourse) {
-            await courseModel.updateOne({_id: courseId}, {$set: {title: req.body.title}});
-            return res.status(200).send({message: `Course ${courseId} has been updated`});
-        } return res.status(500).send({message: `Course ${courseId} does not exist`})
+            await courseModel.updateOne({ _id: courseId }, { $set: { title: req.body.title } });
+            return res.status(200).send({ message: `Course ${courseId} has been updated` });
+        }
+        return res.status(500).send({ message: `Course ${courseId} does not exist` });
     } catch (err) {
-        return res.status(500).send({ message: `Error updating course ${courseId}`});
+        return res.status(500).send({ message: `Error updating course ${courseId}` });
     }
 };
+/***************** END: COURSE MANAGEMENT API ******************/
