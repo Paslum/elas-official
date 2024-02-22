cf5d1b44de923af4f5f25a3c440fda3af10d86d3
Paslum <53943943+Paslum@users.noreply.github.com>
2024-02-22 15:35:36 +0100

    Documentation | Favorite.controller

--- backend/7-notebot/src/controllers/favorite.controller.js
+++ backend/7-notebot/src/controllers/favorite.controller.js
@@ -1,6 +1,21 @@
 const db = require("../models");
 const favoriteModel = db.favorite;
 const noteModel = db.note;
+
+/***************** START: FAVORITE NOTES API *****************
+ * @documentation
+ *
+ * @function getFavNotesByUserId
+ * The `getFavNotesByUserId` function is an asynchronous function that retrieves
+ * favorite notes associated with a specific user ID from the database. It queries
+ * the database for favorite notes with the specified user ID and sends a response
+ * with the found notes if available, or a message indicating that no favorite notes
+ * were found.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and a message.
+ * If favorite notes are found, it includes the notes in the response.
+ */
 export const getFavNotesByUserId = async (req, res) => {
     // Extract user ID from request parameters
     const userId = req.params.userId;
@@ -32,17 +47,41 @@ export const getFavNotesByUserId = async (req, res) => {
     }
 };
 
+/**
+ * @function isFavNote
+ * The `isFavNote` function is an asynchronous function that checks if a
+ * specific note is favored by a user. It queries the database for a record
+ * matching the provided user ID and note ID and returns whether the note is
+ * favored by the user or not.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating whether the note is favored by the user.
+ */
 export const isFavNote = async (req, res) => {
     try {
         const isFav = await favoriteModel.find({userId: req.query.userId, note: req.query.note});
         return res.status(200).send({
-            isFav: isFav.length > 0,});
+            isFav: isFav.length > 0,
+        });
     } catch (error) {
         return res.status(500).send({
-            message: `Error checking if note is favored`,});
+            message: `Error checking if note is favored`,
+        });
     }
 };
 
+/**
+ * @function addFavNote
+ * The `addFavNote` function is an asynchronous function that adds a note
+ * to a user's favorites. It first checks if the user already has favorites,
+ * if yes, it updates the existing favorites, else it creates a new record
+ * for the user's favorites. It also increments the favorites count for the note.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of adding the note to favorites.
+ */
 export const addFavNote = async  (req, res) => {
     try {
         const existingFavorites = await favoriteModel.find({ userId: req.body.userId });
@@ -71,6 +110,17 @@ export const addFavNote = async  (req, res) => {
     }
 };
 
+/**
+ * @function remFavNote
+ * The `remFavNote` function is an asynchronous function that removes a note
+ * from a user's favorites. It first checks if the user has favorites, if yes,
+ * it updates the existing favorites to remove the note. It also decrements
+ * the favorites count for the note.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of removing the note from favorites.
+ */
 export const remFavNote = async (req, res) => {
     try {
         const existingFavorites = await favoriteModel.find({ userId: req.body.userId });
@@ -93,4 +143,4 @@ export const remFavNote = async (req, res) => {
         return res.status(500).send({ message: `Error updating favorites for note` });
     }
 };
-
+/***************** END: FAVORITE NOTES API ******************/
