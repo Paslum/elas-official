5ae3b037c6954339a714a84b746e3da32b1e196e
Paslum <53943943+Paslum@users.noreply.github.com>
2024-02-22 14:56:42 +0100

    Documentation | User.routes

--- backend/7-notebot/src/routes/user.routes.js
+++ backend/7-notebot/src/routes/user.routes.js
@@ -1,26 +1,19 @@
-/******
- * @Note This is one example, you can create more route files under
- * the routes folder and export them. Make sure you import the routes
- * in the server.js file.
- */
-
 const controller = require("../controllers/user.controller");
 
 /***************** START: INITIALIZE ROUTER MODULE *****************
  * @documentation
  * The code `let userRouter = require("express").Router();`
- * is creating a new instance of the Express Router.
- * The 'userRouter' in this example is acting like
- * a middleware function that allows you to define routes
- * for your application. Make sure to define the middleware,
- * in this case 'userRouter.use()' function is used to
- * define middleware.
+ * creates a new instance of the Express Router named 'userRouter'.
+ * This instance acts as a middleware function allowing the definition
+ * of routes for the application. Middleware can be defined using
+ * 'userRouter.use()' function, as shown here.
  */
 let userRouter = require("express").Router();
 
 userRouter.use(function (req, res, next) {
+  // Middleware function to set Access-Control-Allow-Headers header
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-  next();
+  next(); // Passing control to the next middleware
 });
 /***************** END: INITIALIZE ROUTER MODULE *****************/
 
@@ -29,17 +22,18 @@ userRouter.use(function (req, res, next) {
  * When creating a route, you need to define the HTTP method
  * and the path. The HTTP method can be any of the following:
  * GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS.
- * In the example below, 'userRouter' is used to define the
- * routes. The 'userRouter.get()' method is used to define a
- * GET route. The first parameter '/users/:userId' is the path and
- * the second parameter 'controller.saveUser' is the controller
- * function. ":userId" is a parameter send through the url
- * The controller function is define in the 'user.controller.js' 
- * file under controllers folder.
+ * In this example, 'userRouter' is used to define the routes.
+ * The 'userRouter.get()' method defines a GET route.
+ * The first parameter '/users/:userId' is the route path and
+ * the second parameter 'controller.getUserById' is the controller
+ * function responsible for handling the request.
+ * ":userId" is a parameter sent through the URL.
+ * The controller function is defined in the 'user.controller.js'
+ * file under the controllers folder.
  */
-userRouter.get("/users/:userId", controller.getUserById); //Get userInfo by userId
-userRouter.post("/users", controller.createNewUser); //Create a new user
-userRouter.put("/users/:userId", controller.updateUser); //Update userInfo by userId
+userRouter.get("/users/:userId", controller.getUserById); // Get userInfo by userId
+userRouter.post("/users", controller.createNewUser); // Create a new user
+userRouter.put("/users/:userId", controller.updateUser); // Update userInfo by userId
 
 /***************** END: CREATE ROUTES ****************************/
 
