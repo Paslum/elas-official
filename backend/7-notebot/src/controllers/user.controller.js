e651fd73223a952b71bbd504ff5066246449606a
Paslum <53943943+Paslum@users.noreply.github.com>
2024-02-22 15:22:57 +0100

    Documentation | user.controller

--- backend/7-notebot/src/controllers/user.controller.js
+++ backend/7-notebot/src/controllers/user.controller.js
@@ -5,19 +5,19 @@ const User = db.user;
  * @documentation
  *
  * @function getUserById
- * The function `getUserById` is an asynchronous function that retrieves
+ * The `getUserById` function is an asynchronous function that retrieves
  * a user from a MongoDB database based on their user ID and sends a
  * response with the user information if found, or a message indicating
  * that the user was not found.
- * @param req - The `req` parameter is an object that represents the
- * HTTP request made to the server.  * It contains information such as
+ * @param {Object} req - The `req` parameter is an object that represents the
+ * HTTP request made to the server. It contains information such as
  * the request method, headers, URL, and parameters.
- * @param res - The `res` parameter is the response object that is used
- * to send the response back to  * the client. It contains methods and
+ * @param {Object} res - The `res` parameter is the response object that is used
+ * to send the response back to the client. It contains methods and
  * properties that allow you to control the response, such as setting
  * the status code, sending data, and setting headers.
- * @returns a response object with a status code and a message. If a
- * user is found, it also includes the found user object in the response.
+ * @returns {Promise<void>} - a response object with a status code and a message.
+ * If a user is found, it also includes the found user object in the response.
  */
 export const getUserById = async (req, res) => {
   try {
@@ -29,13 +29,24 @@ export const getUserById = async (req, res) => {
     return res.status(200).send({ message: `User ${userId} not found!` });
   } catch (err) {
     res
-      .status(500)
-      .send({ message: `Error saving user to your MongoDB database` });
+        .status(500)
+        .send({ message: `Error saving user to your MongoDB database` });
     return;
   }
 };
 /***************** END: GET USER INFO USING A CONTROLLER ******************/
 
+/**
+ * @function createNewUser
+ * The `createNewUser` function is an asynchronous function that creates
+ * a new user and saves it to the database. It extracts user information
+ * from the request body and sends a success message if the user is created
+ * successfully, or an error message if there are issues during creation.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of the operation.
+ */
 export const createNewUser = async (req, res) => {
   try {
     let user = new User({
@@ -53,6 +64,19 @@ export const createNewUser = async (req, res) => {
   }
 };
 
+/**
+ * @function updateUser
+ * The `updateUser` function is an asynchronous function that updates
+ * user details based on their user ID. It retrieves the user from the
+ * database, updates the user information with the data from the request
+ * body, and saves the changes. It sends a success message if the update
+ * is successful, or an error message if the user is not found or if
+ * there are issues during the update process.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A response object with a status code and
+ * a message indicating the success or failure of the operation.
+ */
 export const updateUser = async (req, res) => {
   try {
     const userId = req.params.userId;
