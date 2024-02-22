07f4705acb79ebfc40e79389fe5d83cde7df01e0
Paslum <53943943+Paslum@users.noreply.github.com>
2024-02-22 14:58:57 +0100

    Documentation | widget.routes

--- backend/7-notebot/src/routes/widgets.route.js
+++ backend/7-notebot/src/routes/widgets.route.js
@@ -2,19 +2,18 @@ const controller = require("../controllers/widget.controller");
 
 /***************** START: INITIALIZE ROUTER MODULE *****************
  * @documentation
- * The code `let noteRouter = require("express").Router();`
- * is creating a new instance of the Express Router.
- * The 'userRouter' in this example is acting like
- * a middleware function that allows you to define routes
- * for your application. Make sure to define the middleware,
- * in this case 'userRouter.use()' function is used to
- * define middleware.
+ * The code `let widgetRouter = require("express").Router();`
+ * creates a new instance of the Express Router named 'widgetRouter'.
+ * This instance acts as a middleware function allowing the definition
+ * of routes for the application. Middleware can be defined using
+ * 'widgetRouter.use()' function, as shown here.
  */
 let widgetRouter = require("express").Router();
 
 widgetRouter.use(function (req, res, next) {
+    // Middleware function to set Access-Control-Allow-Headers header
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    next();
+    next(); // Passing control to the next middleware
 });
 
 /***************** END: INITIALIZE ROUTER MODULE *****************/
@@ -24,21 +23,22 @@ widgetRouter.use(function (req, res, next) {
  * When creating a route, you need to define the HTTP method
  * and the path. The HTTP method can be any of the following:
  * GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS.
- * In the example below, 'userRouter' is used to define the
- * routes. The 'userRouter.get()' method is used to define a
- * GET route. The first parameter '/users/:userId' is the path and
- * the second parameter 'controller.saveUser' is the controller
- * function. ":userId" is a parameter send through the url
- * The controller function is define in the 'widget.controller.js'
- * file under controllers folder.
+ * In this example, 'widgetRouter' is used to define the routes.
+ * The 'widgetRouter.get()' method defines a GET route.
+ * The first parameter '/widget/:widget' is the route path and
+ * the second parameter 'controller.getWidget' is the controller
+ * function responsible for handling the request.
+ * ":widget" is a parameter sent through the URL.
+ * The controller function is defined in the 'widget.controller.js'
+ * file under the controllers folder.
  */
+widgetRouter.get('/widget/:widget', controller.getWidget); // Fetch a single Widget
 
-widgetRouter.get('/widget/:widget', controller.getWidget); //Fetch a single Widget
+widgetRouter.post('/addWidget/', controller.addWidget); // Add a Widget to DB
 
-widgetRouter.post('/addWidget/', controller.addWidget); //Add a Widget to DB
+widgetRouter.delete('/widget/delete/:widget', controller.deleteWidget); // Delete a Widget
 
-widgetRouter.delete('/widget/delete/:widget', controller.deleteWidget); //Delete a Widget
+/***************** END: CREATE ROUTES ****************************/
 
-
-//export the router
+// Export the router
 module.exports = widgetRouter;
