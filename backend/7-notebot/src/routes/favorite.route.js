cc7c3b7fe96cf7dbde065480e3b20f923df4ebd5
Paslum <53943943+Paslum@users.noreply.github.com>
2024-02-22 15:05:09 +0100

    Documentation | favorite.route

--- backend/7-notebot/src/routes/favorite.route.js
+++ backend/7-notebot/src/routes/favorite.route.js
@@ -2,19 +2,18 @@ const controller = require("../controllers/favorite.controller");
 
 /***************** START: INITIALIZE ROUTER MODULE *****************
  * @documentation
- * The code `let noteRouter = require("express").Router();`
- * is creating a new instance of the Express Router.
- * The 'userRouter' in this example is acting like
- * a middleware function that allows you to define routes
- * for your application. Make sure to define the middleware,
- * in this case 'userRouter.use()' function is used to
- * define middleware.
+ * The code `let favoriteRouter = require("express").Router();`
+ * creates a new instance of the Express Router named 'favoriteRouter'.
+ * This instance acts as a middleware function allowing the definition
+ * of routes for the application. Middleware can be defined using
+ * 'favoriteRouter.use()' function, as shown here.
  */
 let favoriteRouter = require("express").Router();
 
 favoriteRouter.use(function (req, res, next) {
+    // Middleware function to set Access-Control-Allow-Headers header
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    next();
+    next(); // Passing control to the next middleware
 });
 
 /***************** END: INITIALIZE ROUTER MODULE *****************/
@@ -24,23 +23,19 @@ favoriteRouter.use(function (req, res, next) {
  * When creating a route, you need to define the HTTP method
  * and the path. The HTTP method can be any of the following:
  * GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS.
- * In the example below, 'userRouter' is used to define the
- * routes. The 'userRouter.get()' method is used to define a
- * GET route. The first parameter '/users/:userId' is the path and
- * the second parameter 'controller.saveUser' is the controller
- * function. ":userId" is a parameter send through the url
- * The controller function is define in the 'user.controller.js'
- * file under controllers folder.
+ * In this example, 'favoriteRouter' is used to define the routes.
+ * Various HTTP methods are used to define routes for different
+ * functionalities related to favorites.
+ * The controller functions are defined in the 'favorite.controller.js'
+ * file under the controllers folder.
  */
+favoriteRouter.get('/favorite/:userId', controller.getFavNotesByUserId); // Get favorite notes by UserId
+favoriteRouter.get('/isFavNote/', controller.isFavNote); // Check if a note is a favorite
 
-favoriteRouter.get('/favorite/:userId', controller.getFavNotesByUserId); //Get all Notes by UserId
-favoriteRouter.get('/isFavNote/', controller.isFavNote); //Get all Notes by UserId
+favoriteRouter.post('/favorite', controller.addFavNote); // Add a note to favorites
+favoriteRouter.post('/remfavorite', controller.remFavNote); // Remove a note from favorites
 
+/***************** END: CREATE ROUTES ****************************/
 
-favoriteRouter.post('/favorite', controller.addFavNote); // favorite buttons
-
-favoriteRouter.post('/remfavorite', controller.remFavNote); // favorite buttons
-
-
-//export the router
+// Export the router
 module.exports = favoriteRouter;
